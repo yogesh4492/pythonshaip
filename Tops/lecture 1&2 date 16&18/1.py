4215f644240bf963f094b7b75bd555e0cc1f8988
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-09-16 08:59:08 +0530

    deleted:    .prog1.py.swp 	modified:   Tops/lecture 1&2 date 16&18/1.py 	new file:   Tops/lecture 1&2 date 16&18/2.py 	new file:   Tops/lecture 1&2 date 16&18/3.py 	modified:   Tops/lecture 1&2 date 16&18/main.ipynb

--- Tops/lecture 1&2 date 16&18/1.py
+++ Tops/lecture 1&2 date 16&18/1.py
@@ -1,1 +1,14 @@
 print("Hello")
+#airthimatic operator
+
+a=10
+b=20
+
+print(f"num1 ={a} and num2 ={b}")
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(2**5)
---
+++ Tops/lecture 1&2 date 16&18/3.py
@@ -0,0 +1,8 @@
+#  relational operator
+
+print(60 >90)
+print(10<20)
+print(100==100)
+print("python"=="python")
+subject="java"
+print(subject!="python")
