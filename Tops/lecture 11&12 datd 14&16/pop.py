5e95c49196cfccebebc002bf3e24adb8df22f91f
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-10-14 11:48:53 +0530

    modified:   Tops/lecture 11&12 datd 14&16/pop.py

--- Tops/lecture 11&12 datd 14&16/pop.py
+++ Tops/lecture 11&12 datd 14&16/pop.py
@@ -9,8 +9,8 @@ print(len(fruits_list))
 
 index_value=int(input("Enter Index Value= "))
 if index_value>=0 and index_value<len(fruits_list):
-    fruits_list.pop(index_value)
-    print("Element Remove Succesfuuly")
+    res=fruits_list.pop(index_value)
+    print(f"{res} Remove Succesfuuly")
     print(fruits_list)
 else:
     print("Index Position Does Not Exists!!!")
