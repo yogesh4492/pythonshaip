7bbbc4c1c67b13ea7d6f298ce345f91799cf8141
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-12-14 18:33:44 +0530

    modified:   Tops/lecture 25&26 date 27&29/exception handling/10.py 	modified:   Tops/lecture 25&26 date 27&29/exception handling/thery.txt 	modified:   Tops/lecture 25&26 date 27&29/iterator/1.py

--- Tops/lecture 25&26 date 27&29/exception handling/10.py
+++ Tops/lecture 25&26 date 27&29/exception handling/10.py
@@ -5,7 +5,7 @@ try:
     username=input("ENter Username : ")
     password=input("Enter Password: ")
     if len(username)>4:
-        if len(username)>8:
+        if len(password)>8:
             print("valid user")
         else:
             raise validatorException("PAssword length must be 8  or more than 8 character ")
