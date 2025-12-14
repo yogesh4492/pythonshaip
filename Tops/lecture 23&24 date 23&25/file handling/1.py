9b7e86b5677069008ae0f5443a09b584b0a93002
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-12-14 18:39:29 +0530

    modified:   Tops/lecture 23&24 date 23&25/file handling/1.py

--- Tops/lecture 23&24 date 23&25/file handling/1.py
+++ Tops/lecture 23&24 date 23&25/file handling/1.py
@@ -1,4 +1,4 @@
-# if we want to store are records or outputs in fiel using of file handling concept 
+# if we want to store are records or outputs in file using of file handling concept 
 # in file handling w e have 3 modes are there 
 
 # 1) read mode : raise
